b9ee25b6b7b3983c6ee136d31495521d5a11caf8
Ilya Veselov <l3xforever@ls-zenbook-antergos.home.lan>
2016-10-04 04:30:18 +0500

    Making some progress

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
+extern crate rustc_serialize;
+extern crate hyper;
+
+use std::io::Read;
+use rustc_serialize::json;
+use hyper::client::Client;
+
 fn main() {
-    
+    let markets = ["ja-JP", "en-US", "en-UK", "en-AU", "en-NZ", "en-CA", "de-DE", "zh-CN"];
+    let request_url = "https://www.bing.com/HPImageArchive.aspx?format=js&idx=0&n=1&mkt=".to_string() + &markets[0];
+    let http_client = Client::new();
+    let mut response = http_client.get(&request_url).send().unwrap();
+    assert_eq!(response.status, hyper::Ok);
+
+    let mut buffer = String::new();
+    response.read_to_string(&mut buffer);
+
+    let response_json = json::Json::from_str(&buffer.to_string()).unwrap();
 }
